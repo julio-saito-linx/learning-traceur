be4e8e84f0209fa33097c00b81dee851ad4b1380
Julio Makdisse Saito <saitodisse@gmail.com>
2014-10-12 19:58:34 -0300

    source-map-support enabled

--- src/nodeTest.js
+++ src/nodeTest.js
@@ -3,13 +3,7 @@ require('source-map-support').install();
 
 require('traceur');
 
-class Greeter {
-  sayHi(name = 'Anonymous') {
-    console.log(`Hi ${name}!`);
-    throw new Error('an error here!');
-  }
-}
 
-var greeter = new Greeter();
+var nodeApp = require('nodeApp');
+nodeApp.run();
 
-greeter.sayHi();
